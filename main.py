6e5a13d16bf37d39c7d9f8ccbc143a260ad4a78e
Ronaldo Alves <ralvesmj@gmail.com>
2012-10-12 15:13:34 -0300

    criação da estrutura de gerenciamento da base

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python
 #coding: utf-8 
-#DNS.dnslookup('144.246.202.221.score.senderscore.com','A')
-#http://www.rmunn.com/sqlalchemy-tutorial/tutorial.html 
-#http://jucacrispim.wordpress.com/2009/12/07/pequena-introducao-ao-sqlalchemy/
-#http://www.rmunn.com/sqlalchemy-tutorial/tutorial.html
 
 from lib.postpolicy import Policy
 import sys
-from multiprocessing import Queue
+
 
 if __name__ == '__main__':
-    daemon = Policy('/tmp/policy.pid', stdout='/tmp/arqlog', stderr='/tmp/errlog')
+    daemon = Policy('/tmp/policy.pid',
+                    stdout='/tmp/arqlog',
+                    stderr='/tmp/errlog')
 
     if len(sys.argv) == 2:
         if 'start' == sys.argv[1]:
